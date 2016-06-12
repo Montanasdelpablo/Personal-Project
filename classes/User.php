f0b168366be791c1e135b8ad627673edf28042f5
Pablo Cleij <pablocley@hotmail.com>
2016-06-12 21:58:30 +0200

    Added properties class User

--- classes/User.php
+++ classes/User.php
@@ -4,15 +4,37 @@
 * 
 */
 class User { 
-  private $_username; 
 
-  public function __construct($name) { 
-    $this->_username = $name; 
-  } 
+  private $_username; 
+  private $_password;
+  private $_email;
+  public $points = 100;
+
+
+  public function __construct($name, $password) { 
+  	
+	$this->_username = $name; 
+	$this->_password = $password;
+	  } 
+
 
   public function showUser() { 
     return $this->_username; 
-  } 
+  }
+
+  public function showPoints() {
+  	return $this->points;
+  }
+
+  public function setEmail($email) { 
+    $this->_email = $email; 
+  }
+
+  public function addUser($name, $password, $email) {
+
+	
+
+  }
 } 
 
 
