61cffe84c61dd495fceb04ada66081ac726713d2
Pablo Cleij <pablocley@hotmail.com>
2016-06-13 00:39:44 +0200

    Added methods Set, show User ID, Save User

--- classes/User.php
+++ classes/User.php
@@ -9,6 +9,8 @@ class User {
   private $_password;
   private $_email;
   public $points = 100;
+  private $object;
+  private $_userid;
 
 
   public function __construct($name, $password) { 
@@ -16,6 +18,44 @@ class User {
 	$this->_username = $name; 
 	$this->_password = $password;
 	  } 
+
+	  public function setUserid($name, $password) {
+
+	  			$link = mysqli_connect("localhost", "root", "", "lcsmanager");
+
+				if (!$link) {
+						echo "Error: Unable to connect to MySQL." . PHP_EOL;
+					    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+					    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+					    exit;
+					}
+
+
+  				$sql = "SELECT user_id FROM users WHERE username = '$name' AND password = '$password'";
+					
+				// Runt de Query en stopt resultaat in een variabel		
+				$result = mysqli_query($link, $sql);
+
+				// Als er geen resultaat is word er een error gegeven
+				if (!$result) {
+	    		printf("Error: %s\n", mysqli_error($link));
+	    		exit();
+				}
+				$row = mysqli_fetch_assoc($result);
+
+				$this->_userid = $row["user_id"];	 
+
+	  }
+
+public function showUserid() {
+	return $this->_userid;
+}
+
+
+public function buyJemoeki()
+	   {
+	   	$this->points -= 5;
+	   }
 
 
   public function showUser() { 
@@ -30,9 +70,39 @@ class User {
     $this->_email = $email; 
   }
 
-  public function addUser($name, $password, $email) {
+  public function addUser() {
 
 	
+
+  }
+
+  public function saveUser($obj)
+  {				
+
+  				$link = mysqli_connect("localhost", "root", "", "lcsmanager");
+
+				if (!$link) {
+						echo "Error: Unable to connect to MySQL." . PHP_EOL;
+					    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+					    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+					    exit;
+					}
+
+				$obj = mysql_real_escape_string($obj); 
+			
+
+  				$sql = "UPDATE users SET obj_user = '$obj' WHERE user_id = '$this->_userid' ";
+					
+				// Runt de Query en stopt resultaat in een variabel		
+				$result = mysqli_query($link, $sql);
+
+				// Als er geen resultaat is word er een error gegeven
+				if (!$result) {
+	    		printf("Error: %s\n", mysqli_error($link));
+	    		exit();
+				}
+
+				header("Location: index.php");
 
   }
 } 
