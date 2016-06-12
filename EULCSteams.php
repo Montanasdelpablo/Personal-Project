f73718d6b2a451bce9199477e09ec06461694c64
Pablo Cleij <pablocley@hotmail.com>
2016-06-12 18:24:27 +0200

    Added teams to "EU LCS" league

---
+++ EULCSteams.php
@@ -0,0 +1,193 @@
+<?php 
+
+require 'header.php';
+
+
+?>
+
+<div id="maincontent" class="container">
+
+<div id="sidebar" class="container">
+		
+	<div id="sidebarlogo" class="row">
+	
+	<h1> LCS Manager </h1>
+	
+	</div>
+
+	<div id="socialmediaicons" class="row">
+		<ul id="iconlist" class="col-md-12">
+			<li> <img src="img/facebook.png"> </li>
+			<li> <img src="img/twitter.png"> </li>
+			<li> <img src="img/instagram.png"> </li>
+
+
+		</ul>
+
+	</div>
+
+
+	<div id="navigationbar" class="row">
+	
+	<ul id="navigationlist" class="col-md-10">
+		<li> <a href="main.php"> Home </a> </li>
+		<li> <a href="leagues.php"> Start managing </a> </li>
+		<li> <a href=""> My Profile </a> </li>
+		<li> <a href=""> Messages  </a> </li>
+		<li> <a href=""> Help </a> </li>
+		<li> <a href=""> Contact </a> </li>
+	</ul>
+
+	</div>
+
+
+
+</div>
+
+<div id="content" class="container">
+
+<div id="row1" class="row">
+	
+	<div class="team col-md-3">
+	
+	<img src="img/1024px-FC_Schalke_04_Logo.svg.png">
+
+	<div class="row">
+	<button> <a href=""> Choose team </a></button>
+	</div>
+
+	</div>
+
+	<div class="team col-md-3">
+	
+	<img src="img/600px-Fnaticlogo.png">
+	
+	<div class="row">
+	<button> <a href=""> Choose team </a></button>
+	</div>
+	
+	</div>
+
+	<div class="team col-md-3">
+	
+	<img src="img/300px-Gamers2logo_square.png">	
+	
+	<div class="row">
+	<button> <a href=""> Choose team </a></button>
+	</div>
+	
+	</div>
+
+</div>
+
+<div id="row2" class="row">
+	
+	<div class="team col-md-3">
+	
+	<img src="img/300px-Giants_Gaminglogo_square.png">		
+
+	<div class="row">
+	<button> <a href=""> Choose team </a></button>
+	</div>
+
+	</div>
+
+	<div class="team col-md-3">
+		
+	<img src="img/300px-Splyce_Profile.png">	
+
+	<div class="row">
+	<button> <a href=""> Choose team </a></button>
+	</div>
+
+	</div>
+	
+	<div class="team col-md-3">
+	
+	<img src="img/300px-Team_Vitalitylogo_square.png">	
+
+	<div class="row">
+	<button> <a href=""> Choose team </a></button>
+	</div>
+
+	</div>
+
+</div>
+
+<div id="row3" class="row">
+
+	<div class="team col-md-3">
+	
+	<img src="img/300px-Unicorns_Of_Lovelogo_square.png">
+
+	<div class="row">
+	<button> <a href=""> Choose team </a></button>
+	</div>
+
+	</div>
+
+	<div class="team col-md-3">
+	
+	<img src="img/167265_.png">	
+
+	<div class="row">
+	<button> <a href=""> Choose team </a></button>
+	</div>
+
+	</div>
+	
+	<div class="team col-md-3">
+	
+	<img src="img/download1.png">	
+
+	<div class="row">
+	<button> <a href=""> Choose team </a></button>
+	</div>
+
+	</div>
+	
+
+</div>
+
+<div id="row4" class="row">
+
+	<div class="team col-md-3">
+	
+	<img src="img/download.png">
+
+	<div class="row">
+	<button> <a href=""> Choose team </a></button>
+	</div>
+
+	</div>
+
+
+</div>
+
+
+	
+	
+
+
+</div>
+
+<?php 
+
+
+
+
+
+
+
+
+?>
+
+</div>
+
+
+<?php
+
+
+require 'footer.php';
+
+?>
