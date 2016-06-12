d3fb4dbcc5e11ef96dc0ef9b677dea4835ee1fb2
Pablo Cleij <pablocley@hotmail.com>
2016-06-12 18:23:28 +0200

    Added overview page of the leagues

---
+++ leagues.php
@@ -0,0 +1,130 @@
+<?php 
+
+require 'header.php';
+
+
+?>
+
+<div id="maincontent" class="container">
+
+<div id="sidebar" class="container">
+		
+	<div id="sidebarlogo" class="row">
+	
+	<h1> LCS Manager </h1>
+	
+	</div>
+
+	<div id="socialmediaicons" class="row">
+		<ul id="iconlist" class="col-md-12">
+			<li> <img src="img/facebook.png"> </li>
+			<li> <img src="img/twitter.png"> </li>
+			<li> <img src="img/instagram.png"> </li>
+
+
+		</ul>
+
+	</div>
+
+
+	<div id="navigationbar" class="row">
+	
+	<ul id="navigationlist" class="col-md-10">
+		<li> <a href="main.php"> Home </a> </li>
+		<li> <a href="leagues.php"> Start managing </a> </li>
+		<li> <a href=""> My Profile </a> </li>
+		<li> <a href=""> Messages  </a> </li>
+		<li> <a href=""> Help </a> </li>
+		<li> <a href=""> Contact </a> </li>
+	</ul>
+
+	</div>
+
+
+
+</div>
+
+<div id="content" class="container">
+
+<div id="row1" class="row">
+	
+	<div id="eulcs" class="col-md-10">
+		
+		<h1> EU LCS </h1>	
+
+		
+		<button> <a href="EULCSteams.php"> Choose league </a></button>
+
+	</div>
+
+</div>
+
+<div id="row2" class="row">
+	
+	<div id="nalcs" class="col-md-10">
+		
+		<h1> NA LCS </h1>	
+		<p> Still under development </p>
+		
+		<button> <a href="NALCSteams.php"> Choose league </a></button>
+
+	</div>
+
+</div>
+
+<div id="row3" class="row">
+
+	<div id="lck" class="col-md-10">
+		
+		<h1> LCK </h1>	
+		<p> Still under development </p>
+		
+		<button> <a href=""> Choose league </a></button>
+
+	</div>
+
+</div>
+
+
+<div id="row4" class="row">
+
+	<div id="lpl" class="col-md-10">
+		
+		<h1> LCK </h1>	
+		<p> Still under development </p>
+		
+		<button> <a href=""> Choose league </a></button>
+
+	</div>
+
+</div>
+
+
+
+
+	
+	
+
+
+</div>
+
+<?php 
+
+
+
+
+
+
+
+
+?>
+
+</div>
+
+
+<?php
+
+
+require 'footer.php';
+
+?>
